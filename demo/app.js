802e3256879a11501092911b8e03e817a48cfec2
Manzoor Alam <manzoor1996alam@gmail.com>
2021-11-05 17:49:43 +0530

    use modules

--- demo/app.js
+++ demo/app.js
@@ -1,12 +1,13 @@
 const http = require('http');
+var dt = require('./myModule/getDate');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 });
 
 server.listen(port, hostname, () => {
