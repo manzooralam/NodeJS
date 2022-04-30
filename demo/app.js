347e0953f68aac82361774eed6939ef49455ceb0
Manzoor alam <manzoor.alam@thinktac.com>
2022-04-30 23:51:00 +0530

    Callback

--- demo/app.js
+++ demo/app.js
@@ -11,28 +11,24 @@
 
 */
 
- /* 
- http: 
- - Predefined or built-in module
- - Allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP)
- - The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client.
- - Node js as web server
- - Method: createServer(), listen()
- */
+/*
+http: 
+- Predefined or built-in module
+- Allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP)
+- The HTTP module can create an HTTP server that listens to server ports and gives a response back to the client.
+- Node js as web server
+- Method: createServer(), listen()
+*/
 
- /* 
- HTTP Header:
- - If the response from the HTTP server is supposed to be displayed as HTML,
- - you should include an HTTP header with the correct content type:
- - e.g.  res.writeHead(200, {'Content-Type': 'text/html'});
- - first argument of the res.writeHead() method is the status code, 200 means that all is OK,
- - second argument is an object containing the response headers.
-
-
-
-
- */
-const http = require('http');
+/*
+HTTP Header:
+- If the response from the HTTP server is supposed to be displayed as HTML,
+- you should include an HTTP header with the correct content type:
+- e.g.  res.writeHead(200, {'Content-Type': 'text/html'});
+- first argument of the res.writeHead() method is the status code, 200 means that all is OK,
+- second argument is an object containing the response headers.
+*/
+/* const http = require('http');
 // User defined
 var dt = require('./myModule/getDate');
 
@@ -47,4 +43,21 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-}); 
+});  */
+
+
+/* Callback functions */
+
+/* Sync  */
+// var fs = require("fs");
+// var data = fs.readFileSync('input.txt');
+// console.log(data.toString());
+// console.log("End");
+
+/*  Async */
+var fs = require("fs");
+fs.readFile('input.txt', function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});
+console.log("End");  
